70b5201d6290da8daf76f9198ab8e5c9bf179f4f
yotaadachi <yota961219@gmail.com>
2022-05-21 17:58:59 +0900

    call, apply, bindの使い方

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
-console.log('%c[JavaScript]call, apply, bindの使い方'
-  , 'color:red; font-size: 1.5em');
+// console.log('%c[JavaScript]call, apply, bindの使い方'
+//   , 'color:red; font-size: 1.5em');
 
-// 関数が定義された時点でthisの参照先は決まっていない。
-function greet() {
-  console.log(arguments);
-  let slicedArray = [].slice.call(arguments, 0, 1);
-  console.log(slicedArray);
+// // 関数が定義された時点でthisの参照先は決まっていない。
+// function greet() {
+//   console.log(arguments);
+//   let slicedArray = [].slice.call(arguments, 0, 1);
+//   console.log(slicedArray);
 
-  //let hi = `Hi, ${this.name}`;
-  //console.log(hi);
+//   //let hi = `Hi, ${this.name}`;
+//   //console.log(hi);
+// }
+
+// let obj = {name: "Tom"};
+
+// // callは第一引数でthisの参照先を定義できる。
+// greet.apply(obj, [1,2,3]);
+
+// const arry = [1,2,3,4];
+// console.log(Math.min.apply(null, arry));
+
+let myObj = {
+  id: 42,
+  print() {
+    console.log(this);
+
+    setTimeout(() => {
+      console.log(this.id);
+    }, 1000);
+  }
 }
 
-let obj = {name: "Tom"};
-
-// callは第一引数でthisの参照先を定義できる。
-greet.call(obj, 1,2,3);
+myObj.print();
