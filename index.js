7c7b79133a064a48703eab6f60aa5e2e990ed247
yotaadachi <yota961219@gmail.com>
2022-05-21 17:26:35 +0900

    callの使い方

--- index.js
+++ index.js
@@ -1,65 +1,17 @@
-console.log('%c[JavaScript]thisについて学ぼう'
+console.log('%c[JavaScript]call, apply, bindの使い方'
   , 'color:red; font-size: 1.5em');
 
-let myObj =  {
-  id: 2,
-  printId(){
-    console.log(this.id);
-  }
+// 関数が定義された時点でthisの参照先は決まっていない。
+function greet() {
+  console.log(arguments);
+  let slicedArray = [].slice.call(arguments, 0, 1);
+  console.log(slicedArray);
+
+  //let hi = `Hi, ${this.name}`;
+  //console.log(hi);
 }
 
-// myObj.printId();
+let obj = {name: "Tom"};
 
-const sayFoo = function(){
-  console.log(this['foo']);
-}
-
-foo = 'foo';
-
-const mySecondObj = {
-  foo: "I'm in the obj",
-  sayFoo
-}
-
-// グローバルオブジェクト(window Obj)に定義された『foo』が取得される。
-sayFoo();
-
-// 呼び出し元のオブジェクトが取得される。
-mySecondObj.sayFoo();
-
-function MyObj(id){
-  this.id = id;
-}
-MyObj.prototype.printId = function(id) {
-  console.log(this.id);
-}
-
-
-class MyClass {
-  constructor() {
-    this.id = id;
-  }
-
-  printId(id) {
-    console.log(this.id);
-  }
-}
-
-const newInstance = new MyObj(5);
-
-const outer = {
-  func1: function() {
-    console.log(this);
-    _this = this;
-
-    const func2 = function() {
-      console.log(_this);
-
-      const func3 = function() {
-        console.log(_this);
-      }();
-    }();
-  }
-}
-
-outer.func1();
+// callは第一引数でthisの参照先を定義できる。
+greet.call(obj, 1,2,3);
