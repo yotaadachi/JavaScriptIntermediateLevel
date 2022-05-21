5c41fd2a27079cdc6feb7436964d8aed61b7104a
yotaadachi <yota961219@gmail.com>
2022-05-21 16:57:05 +0900

    初期コミット

--- index.js
+++ index.js
@@ -8,4 +8,41 @@ let myObj =  {
   }
 }
 
-myObj.printId();
+// myObj.printId();
+
+const sayFoo = function(){
+  console.log(this['foo']);
+}
+
+foo = 'foo';
+
+const mySecondObj = {
+  foo: "I'm in the obj",
+  sayFoo
+}
+
+// グローバルオブジェクト(window Obj)に定義された『foo』が取得される。
+sayFoo();
+
+// 呼び出し元のオブジェクトが取得される。
+mySecondObj.sayFoo();
+
+function MyObj(id){
+  this.id = id;
+}
+MyObj.prototype.printId = function(id) {
+  console.log(this.id);
+}
+
+
+class MyClass {
+  constructor() {
+    this.id = id;
+  }
+
+  printId(id) {
+    console.log(this.id);
+  }
+}
+
+const newInstance = new MyObj(5);
