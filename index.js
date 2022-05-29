f589ae5a76618a888c5d439201351688f1b5518b
yotaadachi <yota961219@gmail.com>
2022-05-29 15:03:34 +0900

    タグ付きテンプレートリレラルを使ってみよう

--- index.js
+++ index.js
@@ -1,47 +1,17 @@
-console.log('%c[JavaScript]クロージャーについて学ぼう'
+console.log('%c[JavaScript]タグ付きテンプレートリレラルを使ってみよう'
   , 'color:red; font-size: 1.5em');
 
-/**
- * クロージャーの定義
- * クロージャーとは、関数と、その関数が宣言された
- * レキシカルスコープの組み合わせ。
- */
-
-// let counter = 0;
-// increment(); // 1
-// increment(); // 2
-// counter = 0;
-// increment(); // 『3』と出力したいが、途中で変数の値が操作されてしまい、出力されない。
-
-// function increment() {
-//   counter++;
-//   console.log(counter);
-// }
-
-// let increment = (function () {
-
-//   let counter = 0; // Lexical Scope
-
-//   return function() {
-//     counter += 1;
-//     console.log(counter);
-//   }
-// })();
-
-// increment();
-// increment();
-// increment();
-
-function addStringFactory(tail) {
-  function concat(str) {
-    return str + tail;
-  }
-  return concat;
+function b(strings, ...values) {
+  console.log(strings);
+  console.log(values);
+  return strings.reduce((accu, str, i) => {
+    let val = values[i] ? `<b>${values[i]}</b>` : "";
+    console.log(`${accu}${str}${val}`);
+    return `${accu}${str}${val}`;
+  }, '');
 }
 
-let addAs = addStringFactory(' AAAAA');
-let addBs = addStringFactory(' BBBBB');
-
-let str = 'Tom';
-str = addAs(str);
-console.log(str);
+const str1 = "Bob";
+const str2 = "Tom";
+const result = b`${str1} and ${str2}`;
+console.log(result);
