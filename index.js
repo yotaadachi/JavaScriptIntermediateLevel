190ed608ce70ce06ab794c73cbbacf1068695566
yotaadachi <yota961219@gmail.com>
2022-06-02 06:59:27 +0900

    ループを使いこなそう2

--- index.js
+++ index.js
@@ -1,29 +1,44 @@
-console.log('%c[JavaScript]ループを使いこなそう'
+console.log('%c[JavaScript]ループを使いこなそう2'
   , 'color:red; font-size: 1.5em');
 
 const data = [1 ,4, 2, 5, 3];
 const fruits = {
-  banana: 'バナナ',
-  apple: 'りんご',
-  orange: 'オレンジ'
+  banana: 2,
+  apple: 3,
+  orange: 1
 }
 
-Object.prototype.additionalFn = function() {};
+// const newData = data.map((value, index, array) => {
+//   return value * 2;
+// });
 
-let keyFruits = Object.keys(fruits);
+// const newData = Object.keys(fruits).map((value, index, array) => {
+//   return value + ' aaaa';
+// });
 
-// for(let i = 0; i < keyFruits.length; i++) {
-//   console.log(i, fruits[keyFruits[i]]);
-// }
+// filter: returnの条件式がtrueになったもののみ、newDataに加える。
+// const newData = data.filter((value, index, array) => {
+//   return value === 1;
+// });
 
-// for(let i in fruits) {
-//   if(fruits.hasOwnProperty(i)){
-//     console.log(i, fruits[i]);
-//   }
-// }
+// reduce: accuが蓄積、currが現在の値。
+// const newData = data.reduce((accu, curr) => {
+//   console.log(accu, curr);
+//   return accu + curr;
+// });
 
-for(let i of keyFruits) {
-  if(fruits.hasOwnProperty(i)){
-    console.log(i, fruits[i]);
-  }
-}
+/**
+ * sort: 配列自体の並び替えを行う
+ * a-b: 昇順
+ * b-a: 降順
+ */
+const newData = data
+.map(v => v + 1)
+.sort((a, b) => {
+  // return a - b;
+  return b - a;
+});
+
+console.log('data', data);
+console.log('newData', newData);
+
