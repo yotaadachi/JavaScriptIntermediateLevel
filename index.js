ada65a075e8cd184dc40b8dba0ccde029d969364
yotaadachi <yota961219@gmail.com>
2022-06-02 16:28:24 +0900

    非同期操作について学ぼう2(Promise関数)

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
 console.log('%c[JavaScript]非同期操作について学ぼう1(コールバック関数)'
   , 'color:red; font-size: 1.5em');
 
-function wait(callback, num) {
-  setTimeout(() => {
-    console.log(num);
-    callback(num);
-  }, 1000);
-}
-
-wait(num => {
-  num++;
-  wait(num => {
-    num++
-    wait(num => {
-      num++
-
+function wait(num) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log(num);
+      if(num === 1002) {
+        reject(num);
+      } else {
+        resolve(num);
+      }
     }, num);
-  }, num);
-}, 0);
+  });
+}
+wait(1000).then(num => {
+  num++;
+  return wait(num);
+}).then(num => {
+  num++
+  return wait(num);
+}).then(num => {
+  num++
+  return wait(num);
+}).then(num => {
+  num++
+  return wait(num);
+}).catch(num => {
+  console.error(num, 'error');
+})
+// wait(num => {
+//   num++;
+//   wait(num => {
+//     num++
+//     wait(num => {
+//       num++
+
+//     }, num);
+//   }, num);
+// }, 0);
