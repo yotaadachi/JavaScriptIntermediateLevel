f3caf21d0f1f560fcc85cd253963dd6d586072b5
yotaadachi <yota961219@gmail.com>
2022-06-06 21:47:08 +0900

    プロトタイプをクラスに書き換え

--- index.js
+++ index.js
@@ -6,19 +6,39 @@ class Person {
     this.first = first;
     this.last = last;
   }
+
   introduce() {
     console.log('My name is ' + this.first + ' ' + this.last);
-  }
+  };
+
 }
 
 class Japanese extends Person {
   constructor(first, last) {
     super(first, last);
     this.lang = 'ja';
+    this._age = 0;
   }
+
   introduce() {
     console.log('こんにちは ' + this.first + ' ' + this.last);
+  };
+
+  static sayHello(val) {
+    console.log('こんにちは ' + val);
+  }
+
+  set setAge(value) {
+    this._age = value;
+  }
+
+  get getAge() {
+    return this._age;
   }
 }
 let me = new Japanese('First', 'Last');
 me.introduce();
+console.log(me.getAge);
+me.setAge = 10;
+console.log(me.getAge);
+Japanese.sayHello('First');
