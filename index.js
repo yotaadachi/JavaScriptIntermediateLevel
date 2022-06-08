b77d2caa81acbca024bdfb499c869d48bb21ef6e
yotaadachi <yota961219@gmail.com>
2022-06-08 09:59:06 +0900

    ES2019の使い方

--- index.js
+++ index.js
@@ -1,44 +1,17 @@
-console.log('%c[JavaScript]プロトタイプと継承について'
-  , 'color:red; font-size: 1.5em');
+document.addEventListener('DOMContentLoaded', () => {
+  const es = new ES2019();
+  es.printVersion();
+});
 
-class Person {
-  constructor(first, last) {
-    this.first = first;
-    this.last = last;
+class ES2019 {
+
+  #version = 2019;
+
+  #increment() {
+    this.#version++;
   }
-
-  introduce() {
-    console.log('My name is ' + this.first + ' ' + this.last);
-  };
-
-}
-
-class Japanese extends Person {
-  constructor(first, last) {
-    super(first, last);
-    this.lang = 'ja';
-    this._age = 0;
+  printVersion() {
+    this.#increment();
+    console.log(`Hi, my version is ${this.#version}`);
   }
-
-  introduce() {
-    console.log('こんにちは ' + this.first + ' ' + this.last);
-  };
-
-  static sayHello(val) {
-    console.log('こんにちは ' + val);
-  }
-
-  set setAge(value) {
-    this._age = value;
-  }
-
-  get getAge() {
-    return this._age;
-  }
-}
-let me = new Japanese('First', 'Last');
-me.introduce();
-console.log(me.getAge);
-me.setAge = 10;
-console.log(me.getAge);
-Japanese.sayHello('First');
+};
