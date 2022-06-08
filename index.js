57a00645e6bbf87eaf61c6824aed728ecda25c38
yotaadachi <yota961219@gmail.com>
2022-06-08 10:33:27 +0900

    ES2019の使い方

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const es = new ES2019();
+  const es = new ES2019Extended();
   es.printVersion();
 });
 
@@ -7,11 +7,28 @@ class ES2019 {
 
   #version = 2019;
 
+  set version(val) {
+    this.#version = val;
+  }
+
+  get version() {
+    return this.#version;
+  }
+
   #increment() {
     this.#version++;
   }
+
   printVersion() {
     this.#increment();
     console.log(`Hi, my version is ${this.#version}`);
   }
+
 };
+
+class ES2019Extended extends ES2019 {
+
+  printVersion() {
+    console.log(`Hi, my version is ${this.version}`);
+  }
+}
